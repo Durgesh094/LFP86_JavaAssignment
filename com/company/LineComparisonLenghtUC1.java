4945e758b69e123d4159370ca91a05e71da1fdfa
Durgesh094 <durgeshchavan1994@gmail.com>
2021-12-17 12:27:10 +0530

    Added: Line comparison Problem UC wise

---
+++ com/company/LineComparisonLenghtUC1.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class LineComparisonLenghtUC1 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int x1, x2, y1, y2;
+		double distance;
+
+		System.out.print("Enter x1: ");
+		x1 = sc.nextInt();
+		System.out.print("Enter y1: ");
+		y1 = sc.nextInt();
+		System.out.print("Enter x2: ");
+		x2 = sc.nextInt();
+		System.out.print("Enter y2: ");
+		y2 = sc.nextInt();
+
+		distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println("Distance between them = " + distance);
+
+	}
+}
